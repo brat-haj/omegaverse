b86a2cd9c416c95d8ad2adfd1b9372116706c675
Julien Sailly <juliensailly76@gmail.com>
2024-12-01 16:45:28 +0100

    refactor: app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+    const [accessToken, setAccessToken] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const response = await fetch('/api/auth/spotify/token', {
-          credentials: 'include', 
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setAccessToken(data.accessToken);
-        } else {
-          console.error('Failed to fetch access token');
-        }
-      } catch (error) {
-        console.error('Error fetching access token:', error);
-      }
-    };
+    useEffect(() => {
+        const fetchToken = async () => {
+            try {
+                const response = await fetch("/api/auth/spotify/token", {
+                    credentials: "include",
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setAccessToken(data.accessToken);
+                } else {
+                    console.error("Failed to fetch access token");
+                }
+            } catch (error) {
+                console.error("Error fetching access token:", error);
+            }
+        };
 
-    fetchToken();
-  }, []);
+        fetchToken();
+    }, []);
 
-  return (
-    <div>
-      <h1 className='bg-red-700'>Spotify Authentication</h1>
-      {accessToken ? (
-        <p>Access Token: {accessToken}</p>
-      ) : (
-        <>
-        <p>Not authenticated</p>
-        <button onClick={() => window.location.href = 'http://localhost:5000/auth/spotify/login'}>
-          Login with Spotify
-        </button>
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="bg-red-700">Spotify Authentication</h1>
+            {accessToken ? (
+                <p>Access Token: {accessToken}</p>
+            ) : (
+                <>
+                    <p>Not authenticated</p>
+                    <button
+                        onClick={() =>
+                            (window.location.href =
+                                "http://localhost:5000/auth/spotify/login")
+                        }
+                    >
+                        Login with Spotify
+                    </button>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
